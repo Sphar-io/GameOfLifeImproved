3b25b125105dafb31dbf901614e658de52e8b0f1
Sphar-io <Jonathan@sphar.io>
2016-09-24 02:18:30 -0400

    Bugfixing
    Improved version now works! Had to re-upload files

---
+++ betterGol.js
@@ -0,0 +1,80 @@
+//initializes vars
+var canvas, ctx, gamePaused, width, height, gameMode, genCount, mouseOverCanvas, mouseDown, fillAmount;
+
+//initializes variables
+function load(){
+		canvas = document.getElementById("canvasA");
+		ctx = canvas.getContext("2d");
+		canvas.width = Math.floor(.90*window.innerHeight);
+		canvas.height = Math.floor(.90*window.innerHeight);
+		gamePaused=true;
+		curGen = 0;
+}
+
+//every time this is called it re-initializes grid, creating empty arrays. outer array(gen[i]) is columns, inner(gen[i][j]) is rows;
+function initializeGrid() {
+	curGen = [[]];
+	nextGen = [[]];
+	for (var i = 0; i < width; i++) {
+		curGen[i] = [];
+		nextGen[i] = [];
+	}
+}
+
+//fills grid based on fill amount. Higher amount = more fill.
+function fillRandom(){
+	//converts to a decimal
+	var amount = (100-fillAmount)/100;
+	for(var i = 0 ; i < curGen.length; i++){
+		for(var j = 0 ; j < height; j++){
+			(Math.random() > amount) ? curGen[i][j] = 1 : curGen[i][j] = 0;
+		}
+	}
+}
+
+
+function golStep(){ 
+	for(var i=1; i <curGen.length-1; i++){ //This loops doesn't calculate outside edges, wrapping is done seperately
+		for (var j = 1; j < curGen[i].length-1; j++) { 	
+			var numNeighbors = 0;
+			//get curent cell
+			var curCell = curGen[i][j];
+			//neighbor checking
+			numNeighbors += curGen[i-1][j-1]; //upper row
+			numNeighbors += curGen[i-1][j]; 
+			numNeighbors += curGen[i-1][j+1];
+			numNeighbors += curGen[i][j-1]; // center row
+			numNeighbors += curGen[i][j+1];
+			numNeighbors += curGen[i+1][j-1]; //lower row
+			numNeighbors += curGen[i+1][j];
+			numNeighbors += curGen[i+1][j+1];
+			//references method based on gameMode
+			nextGen[i][j] = checksLive(numNeighbors, curCell);		
+		}
+	}
+
+		//swaps current to nextGen
+		var temp = curGen.slice(0);
+		curGen = nextGen.slice(0);
+		nextGen = temp.slice(0);
+}
+
+function checksLive(numNeighbors, curCell){
+	if(gameMode=="Original"){
+		if(curCell &&(numNeighbors==2 || numNeighbors ==3)){
+			return 1;
+		}
+		if(!curCell && numNeighbors ==3){
+			return 1;
+		}
+	}
+	if(gameMode == "Maze"){
+		if(curCell &&(numNeighbors >= 1 && numNeighbors <= 5)){
+			return 1;
+		}
+		if(!curCell && numNeighbors ==3){
+			return 1;
+		} 
+	}
+	return 0;
+}
---
+++ draw.js
@@ -0,0 +1,129 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+	load();
+}, false);
+
+
+//creates objects for gui
+function defaultValObj(){
+	this.randomFill = 75;
+	this.pause = function(){pauseGame();};
+	this.regenerate = function(){regenerateGame();};
+	this.stepAnimation = function(){stepGame();};
+	this.gameMode = 'Original';
+	this.gameSize = 75;
+	regenerateGame();
+	randomFillChange(this.randomFill);
+	gameModeChange(this.gameMode);
+	gameSizeChange(this.gameSize);
+	initializeGrid();
+}
+
+function generatePatterns(){
+	this.acorn = function(){};
+}
+var gui;
+//creates gui items
+window.onload = function() {
+	//create objects for gui
+	var menu = new defaultValObj();
+	var patterns = new generatePatterns();
+	gui = new dat.GUI();
+	//start adding to gui
+	//Folder for simulation settings
+	var f1 = gui.addFolder('Simulation Settings');
+		f1.add(menu, 'gameMode', ['Original', 'Maze']).onChange(gameModeChange);
+		f1.add(menu, 'randomFill', 0, 100).onChange(randomFillChange);
+		f1.add(menu, 'gameSize', 0, 100).onChange(gameSizeChange);
+		//subfolder for patterns
+		var f1s1 = f1.addFolder('Example Patterns');
+			f1s1.add(patterns, 'acorn');
+	//folder for controls
+	var f2 = gui.addFolder('Controls');
+		f2.add(menu, 'pause');
+		f2.add(menu, 'regenerate');
+		f2.add(menu, 'stepAnimation');
+	//end adding to gui
+
+
+	//opens menus
+	f1.open();
+	f2.open();
+
+	//call first tick of program after window is loaded
+	regenerateGame();
+	update();
+}
+
+//functions when a value is changed
+function gameModeChange(value){
+	gameMode = value;
+	fillRandom(); //re-initializes
+	draw();
+}
+
+function randomFillChange(value){
+	fillAmount = value;
+	fillRandom(); //re-initializes
+	draw();
+}
+
+function gameSizeChange(value){
+	var tempSize = value/100 //decimal
+	tempSize = Math.floor(tempSize*canvas.width);
+	width = tempSize;
+	height = tempSize;
+	initializeGrid();
+	regenerateGame();
+	draw();
+}
+
+//functions when a button is pressed
+function pauseGame(){
+	gamePaused = !gamePaused;
+}
+
+function regenerateGame(){
+	stepCount = 0; //restarts
+	canvasData = ctx.createImageData(canvas.width, canvas.height);
+	data = canvasData.data;
+	fillRandom();
+	draw();
+}
+
+function stepGame(){
+	genCount++;
+	golStep();
+	draw();
+}
+
+function draw(){
+	for (var i = 0; i < curGen.length; i++) {
+		for (var j = 0; j < curGen[i].length; j++) {
+			var index = ((canvas.width*j+i)*4);
+			var alpha = 0;
+			if(curGen[i][j]==1){
+				alpha = 255;
+			}
+			data[index+3] = alpha;	
+		}
+	}
+	//move to center of page
+	ctx.putImageData(canvasData,0,0);
+}
+
+
+
+//game step function
+function update() {
+	if(!gamePaused){
+		genCount++;
+		golStep();
+		draw();
+		requestAnimationFrame(update);
+	}else {
+		//try again
+		requestAnimationFrame(update);
+	}
+
+}
